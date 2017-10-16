a013db4ceb1df3d3528666bdee3683432fa2d774
Roverkim <ryankim@hotmail.com>
2017-10-16 15:57:30 -0500

    Modal background blurr added. Autoplay Play-Pause Functionality Added Via Double Click

--- assets/js/slider.js
+++ assets/js/slider.js
@@ -1,11 +1,32 @@
 $(document).ready(function(){
 
+  // Set default of slider to autoplay
   $(".autoplay").slick({
     slidesToShow: 1,
     slidesToScroll: 1,
     autoplay: true,
-    autoplaySpeed: 4000,
+    autoplaySpeed: 1000,
     dots: true
   });
+
+
+  // Event listner for toggling autoplay
+  $("#mainContent").on("dblclick", function(){
+    var state = $("#mainContent").attr("data-state");
+
+    if(state == "true"){
+      $(".autoplay").slick("slickPause");
+      $("#mainContent").attr("data-state", "false");
+      console.log("yes");
+    }
+
+    if(state == "false") {
+      $(".autoplay").slick("slickPlay");
+      $("#mainContent").attr("data-state", "true");
+      console.log("no");
+
+    }
+
+  }); // End of Event listner
 
 });
